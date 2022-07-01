81b7542cc8e702933077cd8cea1d3446e45112a5
varuntej72@gmail.com <varuntej72@gmail.com>
2022-07-01 18:31:18 +0530

    sort(01s)(02s)(03s)

---
+++ Week-6/exercise6.3.js
@@ -0,0 +1,35 @@
+function sortZerosOnesTwos(arr){
+    let low = 0,
+      mid = 0,
+      high = arr.length - 1;
+    while (mid <= high){
+      if(arr[mid] === 0){
+        [arr[mid],arr[low]] = [arr[low],arr[mid]];
+        low++;
+        mid++;
+        continue
+      }
+      if(arr[mid] === 1){
+        mid++;
+        continue
+      }
+      if(arr[mid] === 2){
+        [arr[mid],arr[high]] = [arr[high],arr[mid]];
+        high--;
+        continue
+      }
+      break;
+    }
+    return arr;
+  }
+  
+let arr1 = [0,2,1,0,2,1,0,2]
+let arr2 = [1,0,1,0,1,1]
+let arr3 = [NaN]
+let arr4 = [undefined]
+let arr5= []
+let arr6 = [2,0,1,null, undefined, NaN,0,NaN]
+console.time();
+let result = sortZerosOnesTwos(arr6)
+console.log(result);
+console.timeEnd();s
